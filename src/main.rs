d218d1fe6c119739578db2d17dc67adf1bf76bb8
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-03-19 18:32:40 -0400

    draw mod finished, snake started

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate piston_window;
 extern crate rand;
 
 mod draw;
+mod snake;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/snake.rs
@@ -0,0 +1,7 @@
+use piston_window::types::Color;
+use piston_window::{Context, G2d};
+use std::collections::LinkedList;
+
+use draw::draw_block;
+
+const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0]; // [R,G,B, OPACITY]
